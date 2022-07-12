dede6dd326b571b1374567b4047360ed8a4cf71a
Daryl Ronningen <relms@relms.dev>
2022-07-12 12:28:38 -0700

    feat(backend): added typeorm postgres support

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import configuration from './config/configuration';
 
 @Module({
-	imports: [ConfigModule.forRoot({
-		load: [configuration],
-		isGlobal: true,
-	})],
+	imports: [
+		ConfigModule.forRoot({
+			load: [configuration],
+			isGlobal: true,
+		}),
+		TypeOrmModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: (configService: ConfigService) => ({
+				type: 'postgres',
+				host: configService.get('database.host'),
+				port: configService.get('database.port'),
+				database: configService.get('database.database'),
+				username: configService.get('database.username'),
+				password: configService.get('database.password'),
+				synchronize: true,
+			}),
+		}),
+	],
 })
 export class AppModule { }
